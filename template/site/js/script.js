10df316224a28611531efa701c0ed71a0be7d525
ITGGOT-Emanuel-Swenson <emanuel.svensson@itggot.se>
2017-01-17 15:23:53 +0100

    idag

--- template/site/js/script.js
+++ template/site/js/script.js
@@ -1,3 +1,16 @@
 /**
  * Created by gondola on 1/17/17.
  */
+function chngimg(b) {
+    var logo = document.getElementById('gall');
+    logo.src = b;
+
+}
+function gallery(e) {
+    var logo = document.getElementById('gall');
+    for (i = 0; i < 4; i++) {
+    file = "./img/" + i + ".jpg" ;
+
+    setInterval(function(){ chngimg(file); }, 3000);
+    }
+}
